ecd8694dcc303c03aecc26f339ed5080f62fe017
DevArt002 <devart002@gmail.com>
2020-08-25 08:53:02 +1000

    first commit with 2 box models

---
+++ src/index.js
@@ -0,0 +1,39 @@
+import ReactDOM from 'react-dom'
+import React, { useRef, useState, Fragment } from 'react'
+import { Canvas, useFrame } from 'react-three-fiber'
+
+import './styles.css'
+
+function Box(props) {
+  // This reference will give us direct access to the mesh
+  const mesh = useRef()
+  // Set up state for the hovered and active state
+  const [hovered, setHover] = useState(false)
+  const [active, setActive] = useState(false)
+
+  return (
+    <mesh
+      {...props}
+      ref={mesh}
+      scale={active ? [1.5, 1.5, 1.5] : [1, 1, 1]}
+      onClick={e => setActive(!active)}
+      onPointerOver={e => setHover(true)}
+      onPointerOut={e => setHover(false)}>
+      <boxBufferGeometry attach="geometry" args={[1, 1, 1]} />
+      <meshStandardMaterial attach="material" color={hovered ? 'hotpink' : 'orange'} />
+    </mesh>
+  )
+}
+
+ReactDOM.render(
+  <Fragment>
+    <h1>Hello, this is react-three-fiber!</h1>
+    <Canvas>
+      <ambientLight />
+      <pointLight position={[10, 10, 10]} />
+      <Box position={[-1.2, 0, 0]} />
+      <Box position={[1.2, 0, 0]} />
+    </Canvas>
+  </Fragment>,
+  document.getElementById('root')
+)
